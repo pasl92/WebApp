38d3c814b4973ec19d6d0f6e486c5f2c7db02122
pasl92 <pawel.salata@microsoft.wsei.edu.pl>
2021-04-11 13:46:12 +0200

    create project and api key

---
+++ Lab3/src/app.ts
@@ -0,0 +1,28 @@
+export class App {
+    opwApiKey = '444cac076196e5700436d363ef5480ee';
+    constructor() {
+        this.getCityInfo('zakopane')
+    }
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather('zakopane');
+        this.saveData(weather);
+    }
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        return weatherData;
+    }
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
+    }
+}
